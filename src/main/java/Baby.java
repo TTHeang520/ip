2f33c6931344371821c92b60e3950da257014d64
Tan Teck Heang <tanth2005@gmail.com>
2026-08-20 15:05:00 +0800

    Implement Level 1

--- src/main/java/Baby.java
+++ src/main/java/Baby.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class Baby {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Baby.");
         System.out.println("What can I do for you, your highness.");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println("Bye. I'll miss you.");
+        scanner.close();
     }
 }
 
