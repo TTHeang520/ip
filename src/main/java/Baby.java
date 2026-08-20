fcf536973fc178de4ca2ba29dcd1c6d689264fb6
Tan Teck Heang <tanth2005@gmail.com>
2026-08-20 15:29:14 +0800

    Add support for storing and listing tasks

--- src/main/java/Baby.java
+++ src/main/java/Baby.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Baby {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("Hello! I'm Baby.");
         System.out.println("What can I do for you, your highness.");
@@ -10,12 +12,18 @@ public class Baby {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
+                System.out.println("Bye. I'll miss you.");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
-
-        System.out.println("Bye. I'll miss you.");
         scanner.close();
     }
 }
