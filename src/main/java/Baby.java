8aad9a1b5df35a7a11856d9f18debd2c96af6976
Tan Teck Heang <tanth2005@gmail.com>
2026-08-20 16:31:29 +0800

    Add support for marking tasks as done

--- src/main/java/Baby.java
+++ src/main/java/Baby.java
@@ -3,6 +3,8 @@ public class Baby {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
+
         int taskCount = 0;
 
         System.out.println("Hello! I'm Baby.");
@@ -15,9 +17,19 @@ public class Baby {
                 System.out.println("Bye. I'll miss you.");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
                 }
+            }else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                int index = taskNumber - 1;
+
+                isDone[index] = true;
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[index]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
